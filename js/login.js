3837b91b2715bd8271f8c6c889190287cf74a593
KauanOriene <rm99170@fiap.com.br>
2023-05-10 22:47:21 -0300

    validação por carach/exibe senha

---
+++ js/login.js
@@ -0,0 +1,33 @@
+const inputEmail = document.querySelector("#idEmail");
+
+inputEmail.addEventListener("keyup", ()=>{
+
+    const lblEmail = document.querySelector("label[for='idEmail']");
+
+    if(inputEmail.value.length < 5){
+        inputEmail.setAttribute("style", "outline-color:#ff0000");
+        lblEmail.setAttribute("style", "color:#ff0000");
+    }else{
+        inputEmail.setAttribute("style", "outline-color:#00ff00");
+        lblEmail.setAttribute("style", "color:#00ff00");
+    }
+
+
+});
+
+const eyePass = document.querySelector(".fa");
+eyePass.addEventListener("click", ()=>{
+
+    const inputSenha = document.querySelector("#idPass");
+
+    if(inputSenha.getAttribute("type") == "password"){
+        inputSenha.setAttribute("type","text");
+        eyePass.setAttribute("class","fa fa-eye-slash");
+    }else{
+        inputSenha.setAttribute("type","password");
+        eyePass.setAttribute("class","fa fa-eye");
+
+    }
+
+});
+
--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 
-// // var nome = "Ruan";
-// // let nome2 = "Borís";
+// var nome = "Ruan";
+// let nome2 = "Borís";
 
-// // //Regra 1 : INICIALIZAR NA DECLARAÇÃO;
-// // //Regra 2 : NÃO ALTERAR O SEU VALOR DURANTE O FLUXO DO PROGRAMA;
-// // const PI = 3.14;
+// //Regra 1 : INICIALIZAR NA DECLARAÇÃO;
+// //Regra 2 : NÃO ALTERAR O SEU VALOR DURANTE O FLUXO DO PROGRAMA;
+// const PI = 3.14;
 
-// // var nome1 = "Petroni";
+// var nome1 = "Petroni";
 
-// // if(nome1 != ""){
-// //     let nome1 = "Geovana";
-// // }
+// if(nome1 != ""){
+//     let nome1 = "Geovana";
+// }
 
-// // console.log(nome1);
-// /* <button id="meu-btn">CLICK</button> */
+// console.log(nome1);
+/* <button id="meu-btn">CLICK</button> */
 
 // let botao = document.getElementById("meu-btn");
 // // console.log(botao.textContent);
@@ -31,7 +31,9 @@
 //     g = Math.round(Math.random()*255);
 //     b = Math.round(Math.random()*255);
     
-//     this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
+//     // this.setAttribute("style","background-color:rgb("+r+","+g+","+b+");");
+//     this.setAttribute("style",`background-color:rgb(${r},${g},${b});`);
+
 // });
 
 //ESTUDAR
@@ -40,19 +42,17 @@
 //Array
 //Estruturas de repetição
 
-
-//Declarar um array
-// let frutas = ["banana", "maçã","pêssego","laranja","limão","melancia","kiwi"];
+//Declarar um Array 
+// let frutas = ["banana","maçã","pêssego","laranja","limão","melancia","kiwi"];
 // //Imprimindo o array
 // console.log(frutas);
 // console.table(frutas);
 // console.log(frutas[0]);
 // console.log(frutas[6]);
 
-// //Inserir um item ao final do array com o método push();
+// //Inserir um item ao final do array com o método push(nomeDoItem);
 // frutas.push("manga");
 // console.log(frutas);
-
 // //Inserir um item no início do array com o método unshift(nomeDoItem);
 // frutas.unshift("pitaya");
 // console.log(frutas);
@@ -60,25 +60,50 @@
 // //Remover um item ao final do array com o método pop();
 // frutas.pop();
 // console.log(frutas);
-
-// //Remover um item do início do array com o método pop();
+// //Remover um item no início do array com o método shift();
 // frutas.shift();
 // console.log(frutas);
 
 // //Localizar um item no array utilizando o método indexOf(nomeDoItem);
-// //Obs: Este método retorna o índice do item encontrado. 
-
+// //Obs:Este método retorna o índice do item encontrado.
 // // let indice = frutas.indexOf("laranja");
-// // console.log(`O índice encontrado foi: ${indice} `);
-// // console.log(`O item buscado foi: ${frutas[indice]} `);
+// // console.log(`O índice encontrado foi : ${indice}`);
+// // console.log(`O item buscado foi : ${frutas[indice]}`);
 
 // //Realizando uma remoção de item utilizando o método splice().
-// //O método splice() recebe dois parâmetros, o índice do item que será removido e a quantidade de vezes que este mesmo índice será removido. Ex: splice(ìndice, 1);
-// //Dica: Utilize o método indexOf() para buscar o índice do item no array. 
-
+// //O método splice() recebe dois parâmetros, índice do item que será removido e a quantidade de vezes que este mesmo índice será removido. Ex: splice(indice, 1);
+// //Dica:Utilize o método indexOf() para buscar o índice do item no array.
 // let indice = frutas.indexOf("melancia");
-// console.log(`O item encontraddo antes da remoção foi: ${frutas[indice]} `);
+// console.log(`O item encontrado no índice antes da remoção foi : ${ frutas[indice]}`);
 // frutas.splice(indice,1);
 // console.log(frutas);
-// console.log(`O item encontraddo antes da remoção foi: ${frutas[indice]} `);
+// console.log(`O item encontrado no índice depois da remoção foi : ${ frutas[indice]}`);
 
+// //Concatenação de Arrays
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// console.log("Array 1 : " + nr1);
+// console.log("Array 2 : " + nr2);
+
+// //Concatenando os dois arrays em um novo array.
+// let nr3 = [nr1,nr2];
+// console.log("Array 3 : " + nr3);
+
+// nr3.forEach( (nr)=>{
+//     nr.forEach((n)=>{
+//         console.log("Itens do Array 3 : " + n);
+//     });
+// } );
+//Concatenação de Arrays
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+console.log("Array 1 : " + nr1);
+console.log("Array 2 : " + nr2);
+
+//Concatenando os dois arrays em um novo array com o operador SPREAD( ... )
+let nr3 = [...nr1,...nr2];
+console.log("Array 3 : " + nr3);
+
+nr3.forEach( (nr)=>{
+        console.log(`Itens do Array 3 : ${nr}`);
+} );
